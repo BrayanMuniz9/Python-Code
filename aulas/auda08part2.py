6d0371efa2c3bfa06a31a7f1201e600841549e14
Brayan Muniz da Silva <brayan.muniz18@gmail.com>
2022-12-25 19:19:29 -0300

    Atualização de aulas

--- aulas/auda08part2.py
+++ aulas/auda08part2.py
@@ -1,1 +1,4 @@
-a
+import random
+import emoji
+n = random.randint(1, 10)
+print(emoji.emojize(f"Qualquer numero: {n} :sunglasses:"))
---
+++ desafios/desafio16.py
@@ -0,0 +1,4 @@
+import math
+n = float(input('Digite qualquer numero: '))
+r = math.floor(n)
+print(f'A porção inteira do {n} é {r}')
